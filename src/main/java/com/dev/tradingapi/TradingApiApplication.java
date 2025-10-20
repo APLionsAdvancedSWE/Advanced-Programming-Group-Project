6aac19e48c21eaa423fa96761aabbb5e09bfcf35
vangarde03 <alexcobra7743@gmail.com>
2025-10-20 13:00:24 -0400

    suppressing utility class warning
    another attempt
    
    yet another attempt

--- src/main/java/com/dev/tradingapi/TradingApiApplication.java
+++ src/main/java/com/dev/tradingapi/TradingApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * This class contains the startup of the application.
  */
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class TradingApiApplication {
 
