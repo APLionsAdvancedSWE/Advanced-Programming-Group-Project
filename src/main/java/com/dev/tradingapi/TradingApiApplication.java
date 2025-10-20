eaf3bdb3127b486d971a0b2f66382a89b333fcfc
vangarde03 <alexcobra7743@gmail.com>
2025-10-20 12:57:14 -0400

    springboot entry point added

--- src/main/java/com/dev/tradingapi/TradingApiApplication.java
+++ src/main/java/com/dev/tradingapi/TradingApiApplication.java
@@ -1,1 +1,16 @@
-//1
+package com.dev.tradingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This class contains the startup of the application.
+ */
+@SpringBootApplication
+public class TradingApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TradingApiApplication.class, args);
+  }
+
+}
